c6f54a696eecd192247e0a8291ab14e3c05fef6e
root <root@pc>
2022-09-30 17:36:38 +0200

    6-size.c fix6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ long int longIntType;
 long long int longLongIntType;
 float floatType;
 
-printf("Size of a char: %lu\n byte(s)\n", sizeof(charType));
-printf("Size of an int: %lu\n bytes(s)\n", sizeof(intType));
-printf("Size of a long int: %lu\n bytes(s)\n", sizeof(longIntType));
-print("Size of a long long int: %lu\n bytes(s)\n", sizeof(longLongIntType));
-printf("Size of a float: %lu\n bytes(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(longLongIntType));
+printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
 return (0);
 }
