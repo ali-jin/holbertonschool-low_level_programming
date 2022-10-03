44c8122a6129a3369d3abe0f87c668cfdcc3a3ec
root <root@pc>
2022-10-03 13:48:49 +0200

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch <= 'z', ch != 'q', ch != 'e')
+{
+putchar(ch);
+ch++;
+}
+else
+{
+ch++;
+}
+}
+putchar('\n');
+return (0);
+}
