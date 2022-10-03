9f0df7d588a8c92a8bc8c9d2007b3dcc1c244a0c
root <root@pc>
2022-10-03 15:20:43 +0200

    9-print_comb.c fix newline

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,17 +13,14 @@ int main(void)
 int num = 0;
 while (num <= 9)
 {
+putchar('0' + num);
 if (num < 9)
 {
-putchar('0' + num);
 putchar(',');
 putchar(' ');
 }
-else
-{
-putchar('0' + num);
-}
 num++;
 }
+putchar('\n');
 return (0);
 }
