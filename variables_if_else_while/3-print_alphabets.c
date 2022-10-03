9410bda478338d381eadc46b3ae58785d0b860fc
root <root@pc>
2022-10-03 13:41:25 +0200

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+char ch2 = 'A';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (ch2 < 'Z')
+{
+putchar(ch2);
+ch2++;
+}
+putchar('\n');
+return (0);
+}
