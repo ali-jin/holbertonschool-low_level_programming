ae1987b3a660bb916018f572626b0d156c6a7d82
root <root@pc>
2022-10-03 15:14:15 +0200

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+if (num < 9)
+{
+putchar('0' + num);
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar('0' + num);
+}
+}
+return (0);
+}
