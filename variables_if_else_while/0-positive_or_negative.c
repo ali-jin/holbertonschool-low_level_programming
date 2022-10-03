cf575b2f3048b4cb8d46d2a88f42bb1fd5e97c6f
root <root@pc>
2022-10-03 11:14:28 +0200

    0-positive_or_negative.c fix

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,26 +3,28 @@
 /* more headers goes there */
 
 /**
- * main - Entry point
+ * main-Entry point
  *
- * Return: Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-    if (n > 0)
-    {
-        puts("is positive");
-    }else if (n == 0)
-    {
-        puts("is zero");
-    }else
-    {
-        puts("is negative");
-    }
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+puts("is positive");
+}
+else if (n == 0)
+{
+puts("is zero");
+}
+else
+{
+puts("is negative");
+}
+return (0);
 }
