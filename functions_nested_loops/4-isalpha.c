f9e0aac2c3729242cb9533c2131b25a21b06832b
root <root@pc>
2022-10-04 11:18:34 +0200

    characters function fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /* more headers goes there */
 
 /**
- * _islower - Entry point
+ * _isalpha - Entry point
  * @c: character
  *
  * Return: Always 0 (Success)
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-if (islower(c) == 1)
+if (isalpha(c) == 1 && isalpha(c) == 2)
 {
 putchar('1');
 }
