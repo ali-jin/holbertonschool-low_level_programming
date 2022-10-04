eb9c2029a76a881adea6870b435b5679d5e6a2f1
root <root@pc>
2022-10-04 13:24:14 +0200

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _abs - Entry point
+ * @n: number
+ *
+ * Return: Number (Success)
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = (-1) * n;
+return (n);
+}
+else
+{
+return (n);
+}
+}
