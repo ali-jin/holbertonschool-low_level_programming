e23c90dca429e3494d5893c1a84432cc0c503b76
root <root@pc>
2022-10-04 13:07:06 +0200

    5-sign.c return fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,15 +13,16 @@ int print_sign(int n)
 if (n > 0)
 {
 putchar('+');
-putchar(n);
+return (1);
 }
 else if (n < 0)
 {
 putchar('-');
-putchar(n);
+return (-1);
 }
 else
 {
+putchar('0');
 return (0);
 }
 }
