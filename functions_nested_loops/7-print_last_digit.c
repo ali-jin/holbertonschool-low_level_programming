533ce8bd1aa57c1370c96c3aa95c0668001ac0f6
root <root@pc>
2022-10-04 14:47:31 +0200

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_last_digit - Entry point
+ * @n: number
+ *
+ * Return: Number (Success)
+ */
+int print_last_digit(int n)
+{
+int digit = n % 10;
+_putchar(digit);
+return (digit);
+}
