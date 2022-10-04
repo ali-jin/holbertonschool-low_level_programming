739aacbcfbae352db1c0a5895eba301a58b1853a
root <root@pc>
2022-10-04 11:06:57 +0200

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _islower - Entry point
+ * @c: character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+putchar('1');
+}
+else
+{
+putchar('0');
+}
+return (0);
+}
