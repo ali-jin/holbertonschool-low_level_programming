f749b3ea91b9cd24fcf5d22ae0f991a2c73c1d35
root <root@pc>
2022-10-05 10:57:18 +0200

    11-print_to_98.c fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,16 @@ void print_to_98(int n)
 {
 if (n < 98)
 {
-for (int i = n; i < 98; i++)
+for (n; n < 98; n++)
 {
-if (i < 10)
+if (n < 10)
 {
-_putchar(i);
+_putchar(n);
 }
 else
 {
-_putchar('0' + (i / 10));
-_putchar('0' + (i % 10));
+_putchar('0' + (n / 10));
+_putchar('0' + (n % 10));
 }
 _putchar(',');
 _putchar(' ');
@@ -28,16 +28,16 @@ _putchar(' ');
 }
 else if (n > 98)
 {
-for (int i = n; i > 98; i--)
+for (n; n < 98; n++)
 {
-if (i < 10)
+if (n < 10)
 {
-_putchar(i);
+_putchar(n);
 }
 else
 {
-_putchar('0' + (i / 10));
-_putchar('0' + (i % 10));
+_putchar('0' + (n / 10));
+_putchar('0' + (n % 10));
 }
 _putchar(',');
 _putchar(' ');
