1ae8952731dbba416afe3ccef0f3c281c1e704ba
root <root@pc>
2022-10-06 11:53:56 +0200

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: digit
+ *
+ * Return: 1 or 0 (Success)
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
