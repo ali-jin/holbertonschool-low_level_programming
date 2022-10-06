f3434edc11e7461c0d29ecced2a79895e554e98f
root <root@pc>
2022-10-06 12:08:57 +0200

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ */
+void print_numbers(void)
+{
+for (int i = 0; i < 10; i++)
+{
+_putchar('0' + i);
+}
+_putchar('\n');
+}
