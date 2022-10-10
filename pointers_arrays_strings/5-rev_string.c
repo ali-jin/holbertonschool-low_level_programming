c3f236e441211ab75d2d1c8aeb372fa8c572f391
root <root@pc>
2022-10-10 15:25:09 +0200

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s: string
+ *
+ */
+void rev_string(char *s)
+{
+	int i, j;
+    int n = strlen(s);
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		char ch = s[i];
+		s[i] = s[j];
+		s[j] = ch;
+	}
+}
