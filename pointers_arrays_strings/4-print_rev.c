76031fb7f14041ff34b1ffed5d32cf8c746dc3e5
root <root@pc>
2022-10-10 14:08:25 +0200

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s: string
+ *
+ */
+void print_rev(char *s)
+{
+	int i, l;
+
+	l = strlen(s);
+
+	for (i = l; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
