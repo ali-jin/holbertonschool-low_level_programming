091b437d895b4576b63dd0b83d9e518773b8a73a
root <root@pc>
2022-10-10 17:16:26 +0200

    7-puts_half.c fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -20,6 +20,7 @@ void puts_half(char *str)
 		{
 			_putchar(*(str + i));
 		}
+		_putchar('\n');
 	}
 	else
 	{
@@ -29,6 +30,6 @@ void puts_half(char *str)
 		{
 			putchar(*(str + i));
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
