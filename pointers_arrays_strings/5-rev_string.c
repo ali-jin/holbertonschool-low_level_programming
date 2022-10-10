333db1e80cb8abfe360c7bbd80ad8360971d80d4
root <root@pc>
2022-10-10 15:26:28 +0200

    5-rev_string.c fix betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,12 @@
 void rev_string(char *s)
 {
 	int i, j;
-    int n = strlen(s);
+	int n = strlen(s);
 
 	for (i = 0, j = n - 1; i < j; i++, j--)
 	{
 		char ch = s[i];
+
 		s[i] = s[j];
 		s[j] = ch;
 	}
