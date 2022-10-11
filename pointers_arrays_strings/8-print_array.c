893e0c18a556ebfdce103acfa988706924eee68d
root <root@pc>
2022-10-11 10:15:41 +0200

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: integer
+ * @n: integer
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i, tab;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		tab = *(a + i);
+		printf("%d, ", tab);
+	}
+	tab = *(a + (n - 1));
+	printf("%d \n", tab);
+}
