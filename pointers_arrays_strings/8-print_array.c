fb247bfd73a101b03c19c80636153495ac89c177
root <root@pc>
2022-10-11 11:02:52 +0200

    8-print_array.c fix if

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,18 @@ void print_array(int *a, int n)
 {
 	int i, tab;
 
-	for (i = 0; i < n - 1; i++)
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
 	{
 		tab = *(a + i);
 		printf("%d, ", tab);
 	}
 	tab = *(a + (n - 1));
 	printf("%d\n", tab);
+	}
+	else
+	{
+		putchar('\n');
+	}
 }
