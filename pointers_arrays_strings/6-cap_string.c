c72fbd57d2da582e4dda2a21c8c0512dfe6a76d8
root <root@pc>
2022-10-12 14:36:05 +0200

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer string
+ *
+ * Return: i (Success)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
+ * cap_string - Entry point
+ * @s: pointer
+ *
+ * Return: s (Success)
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	int n = _strlen(s);
+
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			j = i - 1;
+			if (s[j] == ' ' || s[j] == '\t' || s[j] == '\n')
+				s[i] -= 32;
+			if (s[j] == '.' || s[j] == ',' || s[j] == ';' || s[j] == '!' || s[j] == '?')
+				s[i] -= 32;
+			if (s[j] == '"' || s[j] == '(' || s[j] == ')' || s[j] == '{' || s[j] == '}')
+				s[i] -= 32;
+		}
+	}
+	return (s);
+}
