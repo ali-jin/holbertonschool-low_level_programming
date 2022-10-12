818ae597f4cd75e040dc8cc7452f32c83b04a2ab
root <root@pc>
2022-10-12 10:15:09 +0200

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: pointer
+ * @n: integer
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int rev[n];
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		rev[n - 1 - i] = a[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rev[i];
+	}
+}
