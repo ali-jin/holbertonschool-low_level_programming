17c14a2b351a23c4b3f3ce9771ce223088ad5d2a
root <root@pc>
2022-10-12 11:00:20 +0200

    4-rev_array.c fix2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,15 +10,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int rev[n];
-	int i;
+	int i, temp;
+	n = n - 1;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++, n--)
 	{
-		rev[n - 1 - i] = a[i];
-	}
-	for (i = 0; i < n; i++)
-	{
-		a[i] = rev[i];
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
