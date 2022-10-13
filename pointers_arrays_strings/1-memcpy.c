a9d8d6ba8559456c3f0cf1d2f489e5ed6527ae75
root <root@pc>
2022-10-13 12:27:44 +0200

    1-memcpy.c fix i

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *d = dest;
 	char *s = src;
+	int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		d[i] = s[i];
 	return (dest);
 }
