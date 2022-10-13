1413934989c287a228c4d659e3e055417166b3a3
root <root@pc>
2022-10-13 12:26:14 +0200

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: pointer
+ * @src: pointer
+ * @n: unsigned integer
+ *
+ * Return: Always s (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	char *s = src;
+
+	for (int i = 0; i < n; i++)
+		d[i] = s[i];
+	return (dest);
+}
