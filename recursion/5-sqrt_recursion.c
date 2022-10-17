a74ba0323c8d4d9dcf85d161de6d6475d3b8bb93
root <root@pc>
2022-10-17 16:56:28 +0200

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * square - search the square
+ * @a: Integer
+ * @r: Integer
+ *
+ * Return: integer (Success)
+ */
+int square(int a, int r)
+{
+	int res = r * r;
+
+	if (res == a)
+		return (r);
+	if (res > a)
+		return (-1);
+	return (square(a, r + 1));
+}
+
+/**
+ * _sqrt_recursion - give the square
+ * @n: Integer
+ *
+ * Return: integer (Success)
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
