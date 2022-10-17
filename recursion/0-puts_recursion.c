5a9d557856200c050df8d4aa8cb8bf6ec78796c5
root <root@pc>
2022-10-17 10:49:56 +0200

    0-puts_recursion.c fix

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,18 +2,39 @@
 #include "main.h"
 
 /**
+ * _strlen - Entry point
+ * @s: Pointer
+ *
+ * Return: Always i (Success)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+
+	}
+	return (i);
+}
+
+/**
  * _puts_recursion - Entry point
  * @s: Pointer
  *
  */
 void _puts_recursion(char *s)
 {
+	int n = strlen(s);
+
 	if (*s != '\0')
 	{
 		putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
-	else
-		_putchar('\n');
+	else if (*s == s[n])
+		putchar('\n');
 }
