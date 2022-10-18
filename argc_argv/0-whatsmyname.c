39f067fce632970ef1e4b2eb5f456786559a9273
root <root@pc>
2022-10-18 14:01:43 +0200

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * name - prints program name
+ * @argc: Integer
+ * @argv: Pointer
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
