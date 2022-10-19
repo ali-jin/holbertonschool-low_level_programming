e53105e86f68dcb9e0d9c25ae1e91cfad4ac8842
root <root@pc>
2022-10-19 15:36:14 +0200

    4-add.c fix3

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -5,30 +5,6 @@
 #include "main.h"
 
 /**
- * _isdigit - Entry point
- * @c: digit
- * @s: pointer
- *
- * Return: 1 or 0 (Success)
- */
-int _isdigit(int c, char *s)
-{
-	int i, res;
-
-	for (i = 0; i < c; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			res = 1;
-		else
-			res = 0;
-	}
-	if (res == 1)
-		return (1);
-	else
-		return (0);
-}
-
-/**
  * main - adds positive numbers
  * @argc: Integer
  * @argv: Pointer
