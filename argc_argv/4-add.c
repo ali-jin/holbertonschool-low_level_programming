6188d72fbe1e589071f70ff12678212df0cb1b7b
root <root@pc>
2022-10-19 12:12:22 +0200

    isdigit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 #include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c: digit
+ * @s: pointer
+ *
+ * Return: 1 or 0 (Success)
+ */
+int _isdigit(int c, char *s)
+{
+	int i, res;
+
+	for (i = 0; i < c; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			res = 1;
+		else
+			res = 0;
+	}
+	if (res == 1)
+		return (1);
+	else
+		return (0);
+}
 
 /**
  * main - adds positive numbers
@@ -12,24 +36,26 @@
  */
 int main(int argc, char *argv[])
 {
-	char *p;
-	int i, sum = 0;
-	int a;
+	int i, sum, j, n = 0;
 
-	if (argc == 1)
-		return (sum);
-
-	for (i = 1; i < argc; i++)
+	if (argc <= 1)
+		printf("0\n");
+	for (j = 1; j < argc; j++)
 	{
-		a = strtol(argv[i], &p, 10);
-		if (isdigit(a))
-			sum += a;
-		else
+		n = strlen(argv[j]);
+		if (_isdigit(n, argv[j]) == 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
 	}
-	printf("%d\n", sum);
+	if (argc >= 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
