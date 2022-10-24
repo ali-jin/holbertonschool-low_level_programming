d86b976dbbe5433702be896e4f41dd5983f820ef
root <root@pc>
2022-10-24 11:20:11 +0200

    size == 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ptr = malloc((size + 1) * sizeof(char));
-	if (ptr == 0)
+	if (size == 0)
 		return (NULL);
 	i = 0;
 	while (i < size)
