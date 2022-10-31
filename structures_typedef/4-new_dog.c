12e6fee13f460edfb54698c5ac63e3bc943f6abc
root <root@pc>
2022-10-31 13:52:23 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,8 +14,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	Dog = malloc(sizeof(dog_t));
 	if (Dog == NULL)
+	{
 		free(Dog);
 		return (NULL);
+	}
 	Dog->name = name;
 	Dog->age = age;
 	Dog->owner = owner;
