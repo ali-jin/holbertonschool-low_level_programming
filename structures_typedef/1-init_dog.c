5bfe1e21280fbda7b0b6849b85d2c67526f15ffb
root <root@pc>
2022-10-31 11:52:43 +0100

    header

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "dog.h"
 #include "main.h"
+#include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - informations of the dog
@@ -17,5 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
 }
