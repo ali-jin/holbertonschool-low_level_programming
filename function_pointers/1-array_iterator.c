fabfba69634a7121d3aa44a05e46b1cd7bfbad6e
root <root@pc>
2022-11-01 22:50:26 +0100

    exit

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a function on each element of an array
  * @array: array
  * @size: size of an array
  * @action: function pointer
@@ -12,6 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i, j = size;
 
+	if (array == NULL || action == NULL)
+		exit(EXIT_SUCCESS);
 	for (i = 0; i < j; i++)
 		action(array[i]);
 }
