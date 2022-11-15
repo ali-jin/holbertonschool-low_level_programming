f5bc869cdc36ac8dd77e432478a54b8c6100587f
root <root@pc>
2022-11-15 14:35:39 +0100

    return fix

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,8 +10,11 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *last = *head;  /* used in step 5*/
+	listint_t *last = *head;
 	int copy = n;
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n  = copy;
 	new_node->next = NULL;
