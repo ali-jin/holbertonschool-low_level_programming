08b5d7da38db619fd3bf83d9044b70aee849ccea
root <root@pc>
2022-11-15 15:04:41 +0100

    3-add_nodeint_end.c

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,23 +11,21 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *last = *head;
-	int copy = n;
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n  = copy;
+	new_node->n  = n;
 	new_node->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = new_node;
-		return (NULL);
+
+	else
+	{
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
 	}
-
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = new_node;
 	return (*head);
 }
