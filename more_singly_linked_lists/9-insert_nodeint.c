69eebf7f1a191e6d8017a744b0e0fdc634cf6bd1
root <root@pc>
2022-11-16 18:02:16 +0100

    free

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ size_t listint_len(const listint_t *h)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i, len = listint_len(*head);
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t *));
 	listint_t *tmp = *head;
 
 	if (idx > len || head == NULL)
