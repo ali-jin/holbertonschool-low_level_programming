5a138e51a0bf489ef78d08756871ccffb309d7cd
root <root@pc>
2022-11-18 17:53:40 +0100

    fix if condition

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int res;
 
-	if (n < index)
+	if (index > sizeof(unsigned long int))
 		return (-1);
 	res = (n >> index) & 1;
 	return (res);
