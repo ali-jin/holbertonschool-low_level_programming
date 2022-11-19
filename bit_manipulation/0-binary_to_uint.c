ef2cd6d6a19ad38fd9afd8cbd28327e39ba82ed9
root <root@pc>
2022-11-19 17:42:40 +0100

    add a for loop

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Entry point
- * @s: string
- *
- * Return: Always 0 (Success)
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-
-/**
- * _pow - result of x power of y
- * @x: Integer
- * @y: Integer
- *
- * Return: integer (Success)
- */
-int _pow(int x, int y)
-{
-	int power = 1, i;
-
-	for (i = 1; i <= y; ++i)
-	{
-		power = power * x;
-	}
-	return (power);
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: binary number(s)
  *
@@ -45,24 +9,22 @@ int _pow(int x, int y)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int sum = 0;
-	int pow, bin, i;
+	int bin = 1, i;
 
 	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (; b[i] != '\0'; i++)
+	i -= 1;
+	for (; i >= 0; i--)
 	{
-		if (b[i] == '0')
-			bin = 0;
 		if (b[i] == '1')
-			bin = 1;
-		pow = _pow(2, i);
-		sum = sum + (bin * pow);
+			sum += bin;
+		bin = bin * 2;
 	}
 	return (sum);
 }
