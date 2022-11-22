e2c6e24a85bcf80287643369b83a2b94fa6b896c
root <root@pc>
2022-11-22 12:17:45 +0100

    add newNode->prev

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -18,6 +18,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newNode->n = copy;
 	newNode->next = *head;
+	newNode->prev = NULL;
+
+	if ((*head) != NULL)
+		(*head)->prev = newNode
+
 	*head = newNode;
 
 	return (newNode);
