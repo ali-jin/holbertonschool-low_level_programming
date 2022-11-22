65ba81b1aacc6888f274385cda715efe5d11f04e
root <root@pc>
2022-11-22 15:16:50 +0100

    kernel fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -46,7 +46,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
-	
+
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
