7893fffc6744054d781b40a8c24adedee11458a9
root <root@pc>
2022-11-22 13:51:29 +0100

    add len and dlistint_len

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: Pointer list
+ *
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int nbNodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nbNodes++;
+	}
+	return (nbNodes);
+}
+
+/**
  * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: dlistint_t list
  * @index: index of the new node
@@ -10,7 +28,10 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node = head;
-	unsigned int idx = 0;
+	unsigned int idx = 0, len = dlistint_len(head);
+
+	if (index > len)
+		return (NULL);
 
 	while (head != NULL)
 	{
