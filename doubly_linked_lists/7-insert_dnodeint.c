563a194c28eceadb294736f699bccc8e3fac6b75
root <root@pc>
2022-11-22 14:28:13 +0100

    newNode->prev

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -57,6 +57,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newNode->next = tmp->next;
 	newNode->prev = tmp;
 	tmp->next = newNode;
+	tmp->next->prev = newNode;
 
 	return (newNode);
 }
