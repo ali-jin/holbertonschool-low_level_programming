f28a90258d00659f5102c31355645fc8d07c8423
root <root@pc>
2022-11-24 19:21:38 +0100

    betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(copyValue);
 		return (0);
 	}
-	index = key_index(k, ht->size)
+	index = key_index(k, ht->size);
 
 	for (i = index; ht->array[i]; i++)
 	{
