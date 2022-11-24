1cd99e4e717435c885711ab5bf55c75c6b5dc8b3
root <root@pc>
2022-11-24 21:49:11 +0100

    initialize variables later

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,11 +10,13 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	const unsigned char *k = (const unsigned char *)key;
-	unsigned long index = key_index(k, ht->size);
-	hash_node_t *item = ht->array[index];
+	unsigned long index;
+	hash_node_t *item;
 
 	if (ht == NULL)
 		return (NULL);
+	index = key_index(k, ht->size);
+	item = ht->array[index];
 	while (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
