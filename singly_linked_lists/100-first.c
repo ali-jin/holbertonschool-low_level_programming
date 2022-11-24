b3a1d20f6567947989fd4c209e2ae7993a7df0d3
root <root@pc>
2022-11-24 22:12:57 +0100

    update

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void Hare_Tortoise(void) __attribute__ ((constructor));
  */
 void Hare_Tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,
-	\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n")
 }
