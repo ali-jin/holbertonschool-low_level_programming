bd557a0ed713a6f8b13f63d0246212b44c13edd8
root <root@pc>
2022-11-25 14:41:36 +0100

    6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -9,17 +9,24 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	hash_node_t *item;
+	hash_node_t *item, *tmp;
 
-	while (i <= ht->size)
+	item = tmp = NULL;
+	if (ht == NULL)
+		return;
+	while (i < ht->size)
 	{
-		while (item != NULL)
+		if (ht->array[i] != NULL)
 		{
-			item = ht->array[i]->next;
-			free(item->key);
-			free(item->value);
-			free(item);
-			ht->array[i] = item;
+			tmp = ht->array[i];
+			while (item != NULL)
+			{
+				item = tmp;
+				tmp = tmp->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+			}
 		}
 		i++;
 	}
