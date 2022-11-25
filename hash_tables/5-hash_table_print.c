bc64d1832a55e4700ca01f03c43453799a7cb124
root <root@pc>
2022-11-25 13:28:15 +0100

    5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: hash table
+ *
+ * Description: A function that prints a hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, len = ht->size;
+	hash_node_t *item;
+	char *comma = "";
+
+	if (ht != NULL)
+	{
+		printf("{");
+		for (i = 0; i < len; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				item = ht->array[i];
+				printf("%s'%s': '%s'", comma, item->key, item->value);
+				comma = ", ";
+			}
+		}
+		printf("}\n");
+	}
+}
